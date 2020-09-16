71921f3b2f9ebb144b06b9179b3dee171b62fb75
kraksin <kraksin12@gmail.com>
2020-09-16 22:12:16 +0900

    setting GlobalStyle and make Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,16 +1,48 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Header = styled.header`
+    color: white;
+    position: fixed;
+    top:0;
+    left:0;
+    width: 100%;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    padding: 0 10px;
+    background-color: rgba(20, 20, 20, 0.8);
+    box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
+`;
+
+const HeaderListUl = styled.ul`
+    display: flex;  
+`;
+
+const Item = styled.li`
+    width: 50px;
+    text-align: center;
+`;
+
+const SLink = styled(Link)`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 50px;
+`;
 
 export default () => (
-    <header>
-        <ul>
-            <li>
-                <a href='/'>Home</a>
-            </li>
-            <li>
-                <a href='/tv'>Tv</a>
-            </li>
-            <li>
-                <a href='/search'>Search</a>
-            </li>
-        </ul>
-    </header>)
+    <Header>
+        <HeaderListUl>
+            <Item>
+                <SLink to='/'>Home</SLink>
+            </Item>
+            <Item>
+                <SLink to='/tv'>Tv</SLink>
+            </Item>
+            <Item>
+                <SLink to='/search'>Search</SLink>
+            </Item>
+        </HeaderListUl>
+    </Header>)
--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import Header from 'Components/Header';
 import Home from 'Routes/Home';
 import Tv from 'Routes/Tv';
 import Search from 'Routes/Search';
 import Detail from 'Routes/Detail';
 
+
 export default () => (
     <Router>
+        <Header />
         <Switch>
             <Route path='/' exact component={Home} />
             <Route path='/tv' exact component={Tv} />
