699e9ac15888bf3bf4170a05fb069e1e8013a7b8
kraksin <kraksin12@gmail.com>
2020-09-16 21:15:52 +0900

    router setting

---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default () => (
+    <header>
+        <ul>
+            <li>
+                <a href='/'>Home</a>
+            </li>
+            <li>
+                <a href='/tv'>Tv</a>
+            </li>
+            <li>
+                <a href='/search'>Search</a>
+            </li>
+        </ul>
+    </header>)
